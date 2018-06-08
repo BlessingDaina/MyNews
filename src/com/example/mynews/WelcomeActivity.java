4bfbd98b4173bdec77df9d599e620fff26981d0f
BlessingDaina <1026395057@qq.com>
2018-06-08 20:21:26 +0800

    将欢迎界面修改为帧动画实现

--- src/com/example/mynews/WelcomeActivity.java
+++ src/com/example/mynews/WelcomeActivity.java
@@ -2,15 +2,25 @@ package com.example.mynews;
 
 import java.util.Timer;
 import java.util.TimerTask;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.view.MotionEvent;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.widget.ImageView;
 
 
-public class WelcomeActivity extends Activity {
+public class WelcomeActivity extends Activity{
+	AnimationDrawable welcomeAnimation;
+	
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState){
 		 super.onCreate(savedInstanceState);  
@@ -21,21 +31,44 @@ public class WelcomeActivity extends Activity {
 	                WindowManager.LayoutParams.FLAG_FULLSCREEN);  
 	        setContentView(R.layout.welcome_main);  
 	   
-	  
-	        //创建Timer对象  
-	        Timer timer = new Timer();  
-	        //创建TimerTask对象  
-	        TimerTask timerTask = new TimerTask() {  
-	            @Override  
-	            public void run() {  
-	                Intent intent = new Intent(WelcomeActivity.this,MainActivity.class);  
+	        ImageView welcomeImage = (ImageView) this.findViewById(R.id.imageView);
+	        welcomeImage.setBackgroundResource(R.drawable.welcomeanimation);
+	        welcomeAnimation = (AnimationDrawable) welcomeImage.getBackground();
+	        welcomeAnimation.start();
+	        
+	        int duration = 0; 
+
+	        for(int i=0;i<welcomeAnimation.getNumberOfFrames();i++){ 
+
+	            duration += welcomeAnimation.getDuration(i); 
+
+	        } 
+
+	        Handler handler = new Handler(); 
+
+	        handler.postDelayed(new Runnable() { 
+
+	            public void run() { 
+	            	Intent intent = new Intent(WelcomeActivity.this,MainActivity.class);  
 	                startActivity(intent);  
-	                finish();  
-	            }  
-	        };  
-	        //使用timer.schedule（）方法调用timerTask，定时3秒后执行run  
-	        timer.schedule(timerTask, 3000);  
+	                finish();  	
+	            } 
+
+	        }, duration); 
 	}
+	
+	public boolean onTouchEvent(MotionEvent event) {
+		if (event.getAction() == MotionEvent.ACTION_DOWN) {
+			Intent intent = new Intent();  
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("http://www.cartier.cn/?utm_source=baidu&utm_medium=brandzone&utm_content=pc_mainzone_title"));  
+            startActivity(intent);
+            return true;
+        }else
+		return false;
+	    
+    }
+
 
 }
 
